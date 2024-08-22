65766d4cbf5165e557996273a3422b9695bf7011
Paulo Castelo <oluap.castelo@gmail.com>
2024-08-22 09:09:31 -0300

    Delegate Predicate

---
+++ DelegatePredicate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DelegatePredicate.Entities;
+
+namespace DelegatePredicate {
+    internal class Program {
+        static void Main(string[] args) {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+            list.RemoveAll(ProductTest);
+            //list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+        }
+        public static bool ProductTest(Product p) {
+            return p.Price >= 100.0;
+        }
+    }
+}
