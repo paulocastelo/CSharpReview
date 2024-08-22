216b9cbe0faaf5fdfed3c30fae7e89257171a16e
Paulo Castelo <oluap.castelo@gmail.com>
2024-08-22 16:06:39 -0300

    Commit "Relational Algebra Fixation exercises"

---
+++ RelationalAlgebraFixationExercises/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationalAlgebraFixationExercises.Entities {
+    class Employee {
+        private static int nextId = 1;
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() {
+            Id = nextId++;
+        }
+
+        public Employee(string name, double salary) {
+            Id = nextId++;
+            Name = name;
+            Salary = salary;
+        }
+
+        public Employee(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            if (id >= nextId) {
+                nextId = id + 1;
+            }
+        }
+
+        public override string ToString() {
+            return Id + ", " + Name + ", " + Salary;
+        }
+    }
+}
---
+++ RelationalAlgebraFixationExercises/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using RelationalAlgebraFixationExercises.Entities;
+using System.Globalization;
+namespace RelationalAlgebraFixationExercises {
+    internal class Program {
+        static void Main(string[] args) {
+            List<Employee> employees = new List<Employee>();
+            string path = @"C:\projetos\CSharpReview\RelationalAlgebraFixationExercises\in.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        double salary = double.Parse(fields[1]);
+                        employees.Add(new Employee(name, salary));
+                    }
+                }
+
+                // Calculate average salary of employees
+                var avg = employees.Select(e => e.Salary).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine("Average salary: " + avg.ToString("F2"));
+
+                // Calculate average salary of employees whose salary is greater than 2000.0
+                var avg2 = (from p in employees
+                            where p.Salary > 2000.0
+                            select p.Salary).DefaultIfEmpty(0.0).Average();
+                Console.WriteLine(avg2.ToString("F2", CultureInfo.InvariantCulture));
+
+                // Get names of employees whose salary is greater than average
+                var names = employees.Where(e => e.Salary > avg).OrderBy(e => e.Name).Select(e => e.Name);
+                foreach (string name in names) {
+                    Console.WriteLine(name);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
